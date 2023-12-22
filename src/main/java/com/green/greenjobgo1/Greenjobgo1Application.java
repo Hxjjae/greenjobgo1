923d57ba7d6af3c540ca204dca00644e104ae373
hxjjae <jhj02201@gmail.com>
2023-12-22 10:59:51 +0900

    백엔드 작업 생성

---
+++ src/main/java/com/green/greenjobgo1/Greenjobgo1Application.java
@@ -0,0 +1,13 @@
+package com.green.greenjobgo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greenjobgo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greenjobgo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/greenjobgo1/Greenjobgo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greenjobgo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greenjobgo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
