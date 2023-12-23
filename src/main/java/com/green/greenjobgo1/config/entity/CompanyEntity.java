e8fa02d0e2a2914fadf9d840faab0b4b830524a2
서영기 <matep8@knou.ac.kr>
2023-12-23 18:42:12 +0900

    Entity 추가 - 서영기

--- src/main/java/com/green/greenjobgo1/config/entity/CompanyEntity.java
+++ src/main/java/com/green/greenjobgo1/config/entity/CompanyEntity.java
@@ -1,4 +1,34 @@
 package com.green.greenjobgo1.config.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.core.annotation.Order;
+
+@Entity
+@Data
+@Order(1)
+@Table(name = "Company_list")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
 public class CompanyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false,nullable = false,columnDefinition = "BIGINT UNSIGNED",length=100)
+    private Long icompany;
+
+    @Column(nullable = false, length = 20, name = "id")
+    private String id;
+
+    @Column(nullable = false, length = 100, name = "password")
+    private String password;
+
+    @Column(nullable = false, length = 20, name = "role")
+    private String role;
+
+    @Column(nullable = false, length = 10, name = "viewable_yn")
+    private int viewableYn;
 }
--- src/main/java/com/green/greenjobgo1/config/entity/EmplySpOfcEntity.java
+++ src/main/java/com/green/greenjobgo1/config/entity/EmplySpOfcEntity.java
@@ -1,4 +1,37 @@
 package com.green.greenjobgo1.config.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.core.annotation.Order;
+
+@Entity
+@Data
+@Order(1)
+@Table(name = "EmplySpOfc")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
 public class EmplySpOfcEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false,nullable = false,columnDefinition = "BIGINT UNSIGNED",length=100)
+    private Long iemply;
+
+    @Column(nullable = false, length = 20, name = "name")
+    private String name;
+
+    @Column(nullable = false, length = 20, name = "phone")
+    private String phone;
+
+    @Column(length = 20, name = "email")
+    private String email;
+
+    @Column(length = 20, name = "profile_pic")
+    private String profilePic;
+
+    @Column(length = 20, name = "kakaoid")
+    private String kakaoid;
 }
