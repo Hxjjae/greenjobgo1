779e74a50cfcd152749e7c7d175641983bfdc8c0
서영기 <matep8@knou.ac.kr>
2024-01-15 22:44:56 +0900

    Admin serivce 예외처리 테스트- 서영기

--- src/main/java/com/green/greenjobgo1/common/security/config/exception/CommonErrorCode.java
+++ src/main/java/com/green/greenjobgo1/common/security/config/exception/CommonErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    SUBJECT_NULL(HttpStatus.UNPROCESSABLE_ENTITY,"없는 과목입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
