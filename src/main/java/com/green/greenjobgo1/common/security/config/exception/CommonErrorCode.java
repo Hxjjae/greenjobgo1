ea06ea911a0378ecd9e966b5c18181fd07f0874f
서영기 <matep8@knou.ac.kr>
2024-01-23 20:53:16 +0900

    errorcode 타입 HttpStatus -> int로 변경 -서영기

--- src/main/java/com/green/greenjobgo1/common/security/config/exception/CommonErrorCode.java
+++ src/main/java/com/green/greenjobgo1/common/security/config/exception/CommonErrorCode.java
@@ -7,22 +7,22 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-    SUBJECT_NULL(HttpStatus.UNPROCESSABLE_ENTITY,"없는 과목입니다."),
-    EMPLOYEE_NULL(HttpStatus.UNPROCESSABLE_ENTITY,"존재하지 않는 직원입니다."),
+    INVALID_PARAMETER(500, "Invalid parameter included"),
+    RESOURCE_NOT_FOUND(500, "Resource not exists"),
+    INTERNAL_SERVER_ERROR(500, "Internal server error"),
+    SUBJECT_NULL(500,"없는 과목입니다."),
+    EMPLOYEE_NULL(500,"존재하지 않는 직원입니다."),
     //로그인 에러
-    EMAIL_NULL(HttpStatus.UNPROCESSABLE_ENTITY,"존재하지 않는 이메일22222"),
-    ID_NULL(HttpStatus.UNPROCESSABLE_ENTITY,"존재하지 않는 아이디"),
-    PASSWORD_FAILED(HttpStatus.UNPROCESSABLE_ENTITY,"비밀번호 불일치."),
-    EXPIRE_LOGIN(HttpStatus.UNPROCESSABLE_ENTITY,"조회만료"),
-    DELETE_SUCCESS(HttpStatus.OK,"성공적으로 삭제되었습니다."),
+    EMAIL_NULL(402,"존재하지 않는 이메일22222"),
+    ID_NULL(500,"존재하지 않는 아이디"),
+    PASSWORD_FAILED(500,"비밀번호 불일치."),
+    EXPIRE_LOGIN(500,"조회만료"),
+    DELETE_SUCCESS(500,"성공적으로 삭제되었습니다."),
 
-    UPLOAD_FAILED(HttpStatus.NOT_ACCEPTABLE, "파일을 업로드 할 수 없습니다."),
-    DELETE_FAILED(HttpStatus.NOT_ACCEPTABLE, "파일을 삭제할 수 없습니다."),
-    NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "editableYn이 활성화 되지 않았습니다.");
+    UPLOAD_FAILED(500, "파일을 업로드 할 수 없습니다."),
+    DELETE_FAILED(500, "파일을 삭제할 수 없습니다."),
+    NOT_AUTHORIZED(500, "editableYn이 활성화 되지 않았습니다.");
 
-    private final HttpStatus httpStatus;
+    private final int httpStatus;
     private final String message;
 }
--- src/main/java/com/green/greenjobgo1/common/security/config/exception/ErrorCode.java
+++ src/main/java/com/green/greenjobgo1/common/security/config/exception/ErrorCode.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public interface ErrorCode {
     String name();
-    HttpStatus getHttpStatus();
+    int getHttpStatus();
     String getMessage();
 }
