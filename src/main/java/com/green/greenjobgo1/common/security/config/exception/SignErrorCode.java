93228004df46fce83300f53959f656cdf71cf0c0
서영기 <matep8@knou.ac.kr>
2024-02-01 15:04:05 +0900

    admin excel 수정 - 서영기

--- src/main/java/com/green/greenjobgo1/common/security/config/exception/SignErrorCode.java
+++ src/main/java/com/green/greenjobgo1/common/security/config/exception/SignErrorCode.java
@@ -12,10 +12,11 @@ public enum SignErrorCode implements ErrorCode{
     EXPIRE_LOGIN(435,"조회만료"),
     DELETE_SUCCESS(436,"성공적으로 삭제되었습니다."),
 
+
     SUBJECT_NULL(437,"없는 과목입니다."),
     EMPLOYEE_NULL(438,"존재하지 않는 직원입니다."),
-    ID_DUPLICATE(439,"존재하는 아이디 입니다.");
-
+    ID_DUPLICATE(439,"존재하는 아이디 입니다."),
+    FILE_ERROR(445,"파일형식이 맞지 않습니다.");
     private final int httpStatus;
     private final String message;
 }
